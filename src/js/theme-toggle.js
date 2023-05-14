076022d7c9e398532eba92167bbbbd147dd61a1a
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-05-14 13:11:22 +0300

    add header and swiych theme

---
+++ src/js/theme-toggle.js
@@ -0,0 +1,26 @@
+const togleswitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem('theme');
+const theme = {
+  LIGHT: 'light',
+  DARK: 'dark',
+};
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === theme.DARK) {
+    togleswitch.checked = true;
+  }
+}
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', theme.DARK);
+    localStorage.setItem('theme', theme.DARK);
+  } else {
+    document.documentElement.setAttribute('data-theme', theme.LIGHT);
+    localStorage.setItem('theme', theme.LIGHT);
+  }
+}
+
+togleswitch.addEventListener('change', switchTheme, false);
