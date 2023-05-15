3f38ad6c46b8f7fa3f1ca7166d32066344534768
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2023-05-15 20:00:07 +0300

    first commit

--- src/js/support.js
+++ src/js/support.js
@@ -1,1 +1,58 @@
-import {fundationsArr} from "./foundations-array.js";
+import { foundatiionsArr } from './foundations-array.js';
+
+const supportList = document.querySelector('ul.list__support');
+const scrollUpBtn = document.querySelector('.scroll-up-btn');
+const scrollDownBtn = document.querySelector('.scroll-down-btn');
+
+
+scrollUpBtn.addEventListener('click', () => {
+  const listItemHeight = supportList.firstElementChild.offsetHeight;
+  supportList.scrollTo({ top: supportList.scrollTop - listItemHeight, behavior: 'smooth' });
+});
+
+
+scrollDownBtn.addEventListener('click', () => {
+  const listItemHeight = supportList.firstElementChild.offsetHeight;
+  supportList.scrollTo({ top: supportList.scrollTop + listItemHeight, behavior: 'smooth' });
+});
+
+
+function updateScrollButtons() {
+  const listItemHeight = supportList.firstElementChild.offsetHeight;
+  const listHeight = supportList.offsetHeight;
+
+  if (supportList.scrollTop === 0) {
+    scrollUpBtn.style.display = 'none';
+  } else {
+    scrollUpBtn.style.display = 'block';
+  }
+
+  if (supportList.scrollTop + listHeight >= supportList.scrollHeight) {
+    scrollDownBtn.style.display = 'none';
+  } else {
+    scrollDownBtn.style.display = 'block';
+  }
+}
+
+
+supportList.addEventListener('scroll', updateScrollButtons);
+
+foundatiionsArr.forEach(foundation => {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  const img = document.createElement('img');
+
+  link.href = foundation.url;
+  link.target = '_blank';
+  link.rel = 'noopener noreferrer';
+
+  img.src = foundation.img.regular;
+  img.alt = foundation.title;
+
+  link.appendChild(img);
+  listItem.appendChild(link);
+  supportList.appendChild(listItem);
+});
+
+
+updateScrollButtons();
