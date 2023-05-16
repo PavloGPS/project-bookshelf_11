c4d72b716a73f336564935c5c031ce3b65f2555d
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-05-16 17:38:11 +0300

    change svg shopping list

--- src/js/header.js
+++ src/js/header.js
@@ -2,20 +2,51 @@ const pageHome = document.querySelector('[data-page="home-list"]');
 const pageShoppingList = document.querySelector('[data-page="shpng-list"]');
 const logo = document.querySelector('.logo');
 
-pageHome.addEventListener('click', activePageHome);
+// 1-й вариант/////////////////////////////////
 
-function activePageHome() {
-  pageHome.classList.add('active');
-  pageShoppingList.classList.remove('active');
-  return;
-}
+// pageHome.addEventListener('click', activePageHome);
 
-pageShoppingList.addEventListener('click', activePageShpngList);
+// function activePageHome() {
+//   pageHome.classList.add('active');
+//   pageShoppingList.classList.remove('active');
+//   return;
+// }
 
-function activePageShpngList() {
-  pageShoppingList.classList.add('active');
-  pageHome.classList.remove('active');
-  return;
-}
+// pageShoppingList.addEventListener('click', activePageShpngList);
 
-logo.addEventListener('click', activePageHome);
+// function activePageShpngList() {
+//   pageShoppingList.classList.add('active');
+//   pageHome.classList.remove('active');
+//   return;
+// }
+
+// logo.addEventListener('click', activePageHome);
+
+// /2-й вариант//////////////////////////////
+// console.log(window.location.href)
+
+// pageHome.addEventListener('click', activePageHome);
+
+// function activePageHome() {
+//   if (document.location.href="http://localhost:1234") {
+//     pageHome.style.backgroundColor = "#EAC645";
+//   } else{
+//     pageShoppingList.style.backgroundColor = "#EAC645"
+//   }
+//   return;
+//   }
+
+// 3-й вариант/////////////////////////////
+
+// pageHome.addEventListener('click', activePageHome);
+
+// function activePageHome() {
+//   if (document.location.href="http://localhost:1234") {
+//   pageShoppingList.classList.remove('active');
+//   pageHome.classList.add('active');
+//   } else{
+//     pageShoppingList.classList.add('active');
+//     pageHome.classList.remove('active');
+//   }
+//   return;
+//   }
