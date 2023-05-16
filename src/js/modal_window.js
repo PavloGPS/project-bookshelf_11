5f8969e2ed478ff24a2bca0cfd79fb4824b3dc21
podletsky <nazar.org@gmail.com>
2023-05-16 23:23:48 +0300

    minimum style+

--- src/js/modal_window.js
+++ src/js/modal_window.js
@@ -1,11 +1,15 @@
 async function serviceDetailInfo(id) {
   const URL = `https://books-backend.p.goit.global/books/${id}`;
   const response = await fetch(URL);
+  console.log(response)
   if (!response.ok) {
     throw new Error(response.statusText);
   }
   return response.json();
 }
+
+
+
 
 serviceDetailInfo('643282b1e85766588626a087')
   .then(data => {
@@ -16,15 +20,17 @@ serviceDetailInfo('643282b1e85766588626a087')
     modalControlBTN.addEventListener('click', e => {
       const bookID = e.target.dataset.id;
       const shoppingList =
-        JSON.parse(localStorage.getItem('shoppingList')) || [];
+        JSON.parse(localStorage.getItem('localShoppingList')) || [];
       if (e.target.dataset.action === "add") {
         console.log(shoppingList);
         shoppingList.push(data);
-        localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+        localStorage.setItem('localShoppingList', JSON.stringify(shoppingList));
+        
       } else {
         const idx = shoppingList.findIndex(({ id }) => id === bookID);
         shoppingList.splice(idx, 1);
-        localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+        localStorage.setItem('localShoppingList', JSON.stringify(shoppingList));
+        
       }
     
     });
@@ -38,8 +44,8 @@ class Modal {
   create(murkup) {
     const html = `<div class="overlay js-module-overlay">
       <div class="modal-wrapper js-close">
-        <div class="modal">
-          <div class="modal-close js-close"></div>
+        <div class="modal">          
+          <button type="button" class="modal-close js-close"></button>
           <div class="modal-content">
           ${murkup}
           </div>
@@ -57,33 +63,33 @@ class Modal {
     this.visible = true;
     const body = document.querySelector('body');
     body.insertAdjacentHTML('afterbegin', this.html);
-    this.#close();
+    this.close();
   }
 
-  #close() {
+  close() {
     const overlay = document.querySelector('.js-module-overlay');
-    this.cleanerEscape = this.#hendlerEscape.bind(this);
-    this.cleanerClick = this.#hendlerClick.bind(this);
+    this.cleanerEscape = this.hendlerEscape.bind(this);
+    this.cleanerClick = this.hendlerClick.bind(this);
     overlay.addEventListener('click', this.cleanerClick);
     document.addEventListener('keydown', this.cleanerEscape);
   }
-  #hendlerClick(e) {
+  hendlerClick(e) {
     if (!e.target.classList.contains('js-close')) {
       return;
     }
     e.currentTarget.remove();
-    this.#cleanEvent();
+    this.cleanEvent();
   }
 
-  #hendlerEscape(e) {
+  hendlerEscape(e) {
     if (e.code === 'Escape') {
       const overlay = document.querySelector('.js-module-overlay');
       overlay.remove();
-      this.#cleanEvent();
+      this.cleanEvent();
     }
   }
 
-  #cleanEvent() {
+  cleanEvent() {
     document.removeEventListener('keydown', this.cleanerEscape);
     this.visible = false;
   }
@@ -91,7 +97,7 @@ class Modal {
 
 function createBookMurkup({ author, book_image, description, title, _id }) {
   const shoppingList = JSON.parse(localStorage.getItem('shoppingList') || '[]');
-  const isInShoppingList = shoppingList.some(item => item._id === _id);
+  const isInShoppingList = shoppingList.some(item => item.id === id);
   return `<div class="modal-container">
       <img src="${book_image}" width="192"  height="281" alt="${title}" />
       <h2>${title}</h2>
@@ -104,8 +110,11 @@ function createBookMurkup({ author, book_image, description, title, _id }) {
       
       <button data-id="${_id}" data-action="${
     isInShoppingList ? 'remove' : 'add'
-  }" type="button" class="add-to-cart-btn">${
+  }" type="button" class="add-to-cart-btn js-close">${
     isInShoppingList ? 'REMOVE FROM THE SHOPPING LIST' : 'ADD TO SHOPPING LIST'
   }</button>
   </div>`;
 }
+
+
+export{serviceDetailInfo}
