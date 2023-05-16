6e5dbbdc9726740e57c2987ab6eb9e922518e16b
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-05-17 01:39:49 +0300

    makes hilighting of current page link
    remove old versions of trying to do it
    repoves class active from elements
    adds class js-link

--- src/js/header.js
+++ src/js/header.js
@@ -1,86 +1,13 @@
-const pageHome = document.querySelector('[data-page="home-list"]');
-const pageShoppingList = document.querySelector('[data-page="shpng-list"]');
-const logo = document.querySelector('.logo');
+const homeLink = document.querySelector('[data-page="home-list"]');
+const ShoppingListLink = document.querySelector('[data-page="shpng-list"]');
 
+const navHreffsArr = document.querySelectorAll('.js-link');
 
-
-// 1-й вариант/////////////////////////////////
-
-// pageHome.addEventListener('click', activePageHome);
-///// document.removeEventListener('keydown', this.cleanerEscape)
-// function activePageHome() {
-
-// }
-
-// pageShoppingList.addEventListener('click', activePageShpngList);
-
-// function activePageShpngList() {
-//   pageShoppingList.classList.add('active');
-//   pageHome.classList.remove('active');
-//   return;
-// }
-
-// logo.addEventListener('click', activePageHome);
-
-// /2-й вариант//////////////////////////////
-console.log(window.location.href)
-
-// pageHome.addEventListener('click', activePageHome);
-
-// function activePageHome() {
-//   if (document.location.href="http://localhost:1234/index.html") {
-//     pageHome.style.backgroundColor = "#EAC645";
-//   } else{
-//     pageShoppingList.style.backgroundColor = "#EAC645"
-//   }
-//   return;
-//   }
-
-// 3-й вариант/////////////////////////////
-
-// pageHome.addEventListener('click', activePageHome);
-
-// function activePageHome() {
-//   if (document.location.href="http://localhost:1234") {
-//   pageShoppingList.classList.remove('active');
-//   pageHome.classList.add('active');
-//   } else{
-//     pageShoppingList.classList.add('active');
-//     pageHome.classList.remove('active');
-//   }
-//   return;
-//   }
-
-
-// 4-й вариант
-pageShoppingList.addEventListener("click", onPageShoppingListClick);
-
-
-function onPageShoppingListClick(){
-    colorSwitch = setTimeout(()=>{
-        pageShoppingList.classList.add('active');
-        pageHome.classList.remove('active');
-    },500);
-    
-    pageShoppingList.removeEventListener('click', onPageShoppingListClick)
-    pageHome.addEventListener('click', onPageHomeBtn)
-}
-function onPageHomeBtn(){
-    colorSwitch = setTimeout(()=>{
-        pageShoppingList.classList.remove('active');
-        pageHome.classList.add('active');
-    },500);
-    
-    pageHome.removeEventListener('click', onPageHomeBtn );
-    document.addEventListener('click', onPageShoppingListClick);
-}
-// http://localhost:1234/index.html
-// //5варіант
-// pageShoppingList.addEventListener('click', activePageShoppingList);
-
-// function activePageShoppingList() {
-
-//   pageShoppingList.classList.toggle('active');  
-//   pageHome.classList.toggle('active');
-  
-//   }
+console.dir(navHreffsArr);
+navHreffsArr.forEach((navLink) => {
+    if (navLink.href===navLink.baseURI) {
+        navLink.style.backgroundColor = "#EAC645";
+    } else {
+        navLink.style.backgroundColor = "transparent";
+    }    
+})
