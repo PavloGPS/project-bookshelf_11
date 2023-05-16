8953255c750d4c24ed86ce437cc8f0917430a9ca
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-05-16 20:50:06 +0300

    trying t o fix hiliting of current page
    add two not working way to hilight current page

--- src/js/header.js
+++ src/js/header.js
@@ -2,14 +2,14 @@ const pageHome = document.querySelector('[data-page="home-list"]');
 const pageShoppingList = document.querySelector('[data-page="shpng-list"]');
 const logo = document.querySelector('.logo');
 
+
+
 // 1-й вариант/////////////////////////////////
 
 // pageHome.addEventListener('click', activePageHome);
-
+///// document.removeEventListener('keydown', this.cleanerEscape)
 // function activePageHome() {
-//   pageHome.classList.add('active');
-//   pageShoppingList.classList.remove('active');
-//   return;
+
 // }
 
 // pageShoppingList.addEventListener('click', activePageShpngList);
@@ -23,12 +23,12 @@ const logo = document.querySelector('.logo');
 // logo.addEventListener('click', activePageHome);
 
 // /2-й вариант//////////////////////////////
-// console.log(window.location.href)
+console.log(window.location.href)
 
 // pageHome.addEventListener('click', activePageHome);
 
 // function activePageHome() {
-//   if (document.location.href="http://localhost:1234") {
+//   if (document.location.href="http://localhost:1234/index.html") {
 //     pageHome.style.backgroundColor = "#EAC645";
 //   } else{
 //     pageShoppingList.style.backgroundColor = "#EAC645"
@@ -50,3 +50,37 @@ const logo = document.querySelector('.logo');
 //   }
 //   return;
 //   }
+
+
+//4-й вариант
+// pageShoppingList.addEventListener("click", onPageShoppingListClick);
+
+
+// function onPageShoppingListClick(){
+//     colorSwitch = setTimeout(()=>{
+//         pageShoppingList.classList.add('active');
+//         pageHome.classList.remove('active');
+//     },500);
+    
+//     pageShoppingList.removeEventListener('click', onPageShoppingListClick)
+//     pageHome.addEventListener('click', onPageHomeBtn)
+// }
+// function onPageHomeBtn(){
+//     colorSwitch = setTimeout(()=>{
+//         pageShoppingList.classList.remove('active');
+//         pageHome.classList.add('active');
+//     },500);
+    
+//     pageHome.removeEventListener('click', onPageHomeBtn );
+//     document.addEventListener('click', onPageShoppingListClick);
+// }
+// http://localhost:1234/index.html
+//5v
+pageShoppingList.addEventListener('click', activePageShoppingList);
+
+function activePageShoppingList() {
+
+  pageShoppingList.classList.toggle('active');  
+  pageHome.classList.toggle('active');
+  
+  }
--- src/js/modal_window.js
+++ src/js/modal_window.js
@@ -1,114 +1,114 @@
-async function serviceDetailInfo(id) {
-  const URL = `https://books-backend.p.goit.global/books/${id}`;
-  const response = await fetch(URL);
-  if (!response.ok) {
-    throw new Error(response.statusText);
-  }
-  return response.json();
-}
+// async function serviceDetailInfo(id) {
+//   const URL = `https://books-backend.p.goit.global/books/${id}`;
+//   const response = await fetch(URL);
+//   if (!response.ok) {
+//     throw new Error(response.statusText);
+//   }
+//   return response.json();
+// }
 
-serviceDetailInfo('643282b1e85766588626a087')
-  .then(data => {
-    const instance = new Modal();
-    instance.create(createBookMurkup(data));
-    instance.open();
-    const modalControlBTN = document.querySelector('.add-to-cart-btn');
-    modalControlBTN.addEventListener('click', e => {
-      const bookID = e.target.dataset.id;
-      const shoppingList =
-        JSON.parse(localStorage.getItem('localShoppingList')) || [];
-      if (e.target.dataset.action === "add") {
-        console.log(shoppingList);
-        shoppingList.push(data);
-        localStorage.setItem('localShoppingList', JSON.stringify(shoppingList));
+// serviceDetailInfo('643282b1e85766588626a087')
+//   .then(data => {
+//     const instance = new Modal();
+//     instance.create(createBookMurkup(data));
+//     instance.open();
+//     const modalControlBTN = document.querySelector('.add-to-cart-btn');
+//     modalControlBTN.addEventListener('click', e => {
+//       const bookID = e.target.dataset.id;
+//       const shoppingList =
+//         JSON.parse(localStorage.getItem('localShoppingList')) || [];
+//       if (e.target.dataset.action === "add") {
+//         console.log(shoppingList);
+//         shoppingList.push(data);
+//         localStorage.setItem('localShoppingList', JSON.stringify(shoppingList));
         
-      } else {
-        const idx = shoppingList.findIndex(({ id }) => id === bookID);
-        shoppingList.splice(idx, 1);
-        localStorage.setItem('localShoppingList', JSON.stringify(shoppingList));
+//       } else {
+//         const idx = shoppingList.findIndex(({ id }) => id === bookID);
+//         shoppingList.splice(idx, 1);
+//         localStorage.setItem('localShoppingList', JSON.stringify(shoppingList));
         
-      }
+//       }
     
-    });
-  })
-  .catch(error => console.log(error));
+//     });
+//   })
+//   .catch(error => console.log(error));
 
-class Modal {
-  constructor() {
-    this.visible = false;
-  }
-  create(murkup) {
-    const html = `<div class="overlay js-module-overlay">
-      <div class="modal-wrapper js-close">
-        <div class="modal">          
-          <button type="button" class="modal-close js-close"></button>
-          <div class="modal-content">
-          ${murkup}
-          </div>
-        </div>
-      </div>
-    </div>`;
+// class Modal {
+//   constructor() {
+//     this.visible = false;
+//   }
+//   create(murkup) {
+//     const html = `<div class="overlay js-module-overlay">
+//       <div class="modal-wrapper js-close">
+//         <div class="modal">          
+//           <button type="button" class="modal-close js-close"></button>
+//           <div class="modal-content">
+//           ${murkup}
+//           </div>
+//         </div>
+//       </div>
+//     </div>`;
 
-    this.html = html;
-  }
+//     this.html = html;
+//   }
 
-  open() {
-    if (this.visible) {
-      return;
-    }
-    this.visible = true;
-    const body = document.querySelector('body');
-    body.insertAdjacentHTML('afterbegin', this.html);
-    this.close();
-  }
+//   open() {
+//     if (this.visible) {
+//       return;
+//     }
+//     this.visible = true;
+//     const body = document.querySelector('body');
+//     body.insertAdjacentHTML('afterbegin', this.html);
+//     this.close();
+//   }
 
-  close() {
-    const overlay = document.querySelector('.js-module-overlay');
-    this.cleanerEscape = this.hendlerEscape.bind(this);
-    this.cleanerClick = this.hendlerClick.bind(this);
-    overlay.addEventListener('click', this.cleanerClick);
-    document.addEventListener('keydown', this.cleanerEscape);
-  }
-  hendlerClick(e) {
-    if (!e.target.classList.contains('js-close')) {
-      return;
-    }
-    e.currentTarget.remove();
-    this.cleanEvent();
-  }
+//   close() {
+//     const overlay = document.querySelector('.js-module-overlay');
+//     this.cleanerEscape = this.hendlerEscape.bind(this);
+//     this.cleanerClick = this.hendlerClick.bind(this);
+//     overlay.addEventListener('click', this.cleanerClick);
+//     document.addEventListener('keydown', this.cleanerEscape);
+//   }
+//   hendlerClick(e) {
+//     if (!e.target.classList.contains('js-close')) {
+//       return;
+//     }
+//     e.currentTarget.remove();
+//     this.cleanEvent();
+//   }
 
-  hendlerEscape(e) {
-    if (e.code === 'Escape') {
-      const overlay = document.querySelector('.js-module-overlay');
-      overlay.remove();
-      this.cleanEvent();
-    }
-  }
+//   hendlerEscape(e) {
+//     if (e.code === 'Escape') {
+//       const overlay = document.querySelector('.js-module-overlay');
+//       overlay.remove();
+//       this.cleanEvent();
+//     }
+//   }
 
-  cleanEvent() {
-    document.removeEventListener('keydown', this.cleanerEscape);
-    this.visible = false;
-  }
-}
+//   cleanEvent() {
+//     document.removeEventListener('keydown', this.cleanerEscape);
+//     this.visible = false;
+//   }
+// }
 
-function createBookMurkup({ author, book_image, description, title, _id }) {
-  const shoppingList = JSON.parse(localStorage.getItem('shoppingList') || '[]');
-  const isInShoppingList = shoppingList.some(item => item._id === _id);
-  return `<div class="modal-container">
-      <img src="${book_image}" width="192"  height="281" alt="${title}" />
-      <h2>${title}</h2>
-      <p>${author}</p>
-      <span>${
-        description
-          ? description
-          : 'We are sorry, we have no description of this book.'
-      }</span>
+// function createBookMurkup({ author, book_image, description, title, _id }) {
+//   const shoppingList = JSON.parse(localStorage.getItem('shoppingList') || '[]');
+//   const isInShoppingList = shoppingList.some(item => item._id === _id);
+//   return `<div class="modal-container">
+//       <img src="${book_image}" width="192"  height="281" alt="${title}" />
+//       <h2>${title}</h2>
+//       <p>${author}</p>
+//       <span>${
+//         description
+//           ? description
+//           : 'We are sorry, we have no description of this book.'
+//       }</span>
       
-      <button data-id="${_id}" data-action="${
-    isInShoppingList ? 'remove' : 'add'
-  }" type="button" class="add-to-cart-btn js-close">${
-    isInShoppingList ? 'REMOVE FROM THE SHOPPING LIST' : 'ADD TO SHOPPING LIST'
-  }</button>
-  </div>`;
-}
+//       <button data-id="${_id}" data-action="${
+//     isInShoppingList ? 'remove' : 'add'
+//   }" type="button" class="add-to-cart-btn js-close">${
+//     isInShoppingList ? 'REMOVE FROM THE SHOPPING LIST' : 'ADD TO SHOPPING LIST'
+//   }</button>
+//   </div>`;
+// }
 
