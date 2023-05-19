466507394cc72d5c73f01f1f13535b5155f132f1
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2023-05-19 06:35:52 +0300

    update

---
+++ src/sass/components/shops-array.js
@@ -0,0 +1,31 @@
+const shopsArr=[
+    {
+      title: 'Amazon',
+      url:
+        'https://www.amazon.com/',
+        img: {
+          regular: new URL('../images/modal/amazon.png', import.meta.url),
+          retina: new URL('../images/modal/amazon@2x.png', import.meta.url),
+        },
+    },
+    {
+      title: 'Book',
+      url: 'https://www.apple.com/apple-books/',
+      img: {
+        regular: new URL('../images/modal/book.png', import.meta.url),
+        retina: new URL('../images/modal/book@2x.png', import.meta.url),
+      },
+    },
+    {
+      title: 'Books',
+      url: '',
+      img: {
+        regular: new URL('../images/modal/books.png', import.meta.url),
+        retina: new URL('../images/modal/books@2x.png', import.meta.url),
+      },
+    }
+  ]
+  
+  export { shopsArr };
+
+  
